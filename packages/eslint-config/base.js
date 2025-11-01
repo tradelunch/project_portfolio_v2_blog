21f1ad00be81f1da0c0e8a222b2f1f14c2ddb442
Taek Lim <tio.taek.lim@gmail.com>
2025-10-31 21:01:40 -0500

    feat: packages update

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import prettierPlugin from "eslint-plugin-prettier";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +17,11 @@ export const config = [
 	{
 		plugins: {
 			turbo: turboPlugin,
+			prettier: prettierPlugin,
 		},
 		rules: {
 			"turbo/no-undeclared-env-vars": "warn",
+			"prettier/prettier": "error",
 		},
 	},
 	{
